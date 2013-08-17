52c9c807093393867c8961bf41b01bc6cb95f719
Samuel Mendenhall <smendenh@redhat.com>
2013-08-17 18:32:48 -0400

    Attempting to fix the lookup of the settings

--- src/dataglue/utils/settings.rb
+++ src/dataglue/utils/settings.rb
@@ -7,7 +7,8 @@ module DataGlueSettings
   def self.config
     if @config.nil?
       if ENV['OPENSHIFT_DATA_DIR']
-        @config = YAML.load_file(File.expand_path('$OPENSHIFT_DATA_DIR/.dataglue-settings.yml'))
+        #@config = YAML.load_file(File.expand_path('$OPENSHIFT_DATA_DIR/.dataglue-settings.yml'))
+        @config = YAML.load_file(File.join(ENV['$OPENSHIFT_DATA_DIR'], '.dataglue-settings.yml'))
       else
         @config = YAML.load_file(File.expand_path('~/.dataglue-settings.yml'))
       end
